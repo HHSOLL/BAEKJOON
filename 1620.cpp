bfbb60cd12e293f5fc52868a860e6ff07491ce5d
HHSOLL <KHSOL3141@gmail.com>
2024-09-26 17:22:02 +0900

    Unordered_map

--- 1620.cpp
+++ 1620.cpp
@@ -1,38 +1,38 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <unordered_map>
+
+#define MIN_LENGTH 2
+#define MAX_LENGTH 20
 
 int main(void) {
   std::ios::sync_with_stdio(false);
   std::cin.tie(NULL);
 
-  int n, m;
-  std::cin >> n >> m;
-  std::vector<std::vector<std::string>> v(n);
+  int N = 0, M = 0;
+  std::cin >> N >> M;
 
-  for (int i = 0; i < n; i++) {
-    std::string str;
+  std::string str;
+  std::unordered_map<std::string, int> table(N + 1);
+  std::unordered_map<int, std::string> table2(N + 1);
+  for (int i = 0; i < N; i++) {
     std::cin >> str;
-    v[i].push_back(str);
+
+    if (str.length() < MIN_LENGTH || str.length() > MAX_LENGTH) exit(0);
+
+    table.insert({str, i + 1});
+    table2.insert({i + 1, str});
   }
 
-  for (int i = 0; i < m; i++) {
-    std::string q;
-    std::cin >> q;
+  for (int i = 0; i < M; i++) {
+    std::cin >> str;
 
-    bool found = false;
-    for (int j = 0; j < n; j++) {
-      if (find(v[j].begin(), v[j].end(), q) != v[j].end()) {
-        std::cout << j + 1 << '\n';
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      int p = stoi(q);
-      std::cout << *v[p - 1].begin() << '\n';
-      // std::cout << "NOTFOUND" << '\n';
+    auto it = table.find(str);
+    if (it != table.end()) {
+      std::cout << it->second << '\n';
+    } else {
+      int k = std::stoi(str) - 1;
+      std::cout << table2.find(k)->second << '\n';
     }
   }
   return 0;
