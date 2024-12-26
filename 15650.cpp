19152e941972fe3e67309306ef4756bef2ea3bcb
HHSOLL <KHSOL3141@gmail.com>
2024-12-27 01:41:14 +0900

    N and M(2)_Backtracking

---
+++ 15650.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+void recur(int n, int m, int start, std::vector<int>& v) {
+  if (v.size() == m) {
+    for (int num : v) {
+      std::cout << num << " ";
+    }
+    std::cout << '\n';
+    return;
+  }
+
+  for (int i = start; i <= n; i++) {
+    v.push_back(i);
+    recur(n, m, i + 1, v);
+    v.pop_back();
+  }
+}
+
+int main(void) {
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<int> v;
+  recur(n, m, 1, v);
+  return 0;
+}
